6a837c3e525be0836e03dc441b9bca227dfebe50
Sudhir Dhawle <dhawlesudhir@gmail.com>
2022-07-14 19:12:10 +0530

    fixes

--- resources/views/apicategories.blade.php
+++ resources/views/apicategories.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.main')
 @section('content')
+@if(count($categories_resources))
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
 <div class="groups">
-  <?php
-  foreach ($categories_resources as $categorie) {
-    if (count($categorie->resources)) {
-  ?>
-      <div class="group" style="border-left: solid 5px #{{$categorie->color}}">
-        <h5 class="group-heading">{{$categorie->name}}</h5>
-        <div class="group-items">
-          <?php
-          foreach ($categorie->resources as $resource) {
-          ?>
-            <a href="">{{$resource->name}}
-              <p>{{$resource->desc}}</p>
-            </a>
-
-          <?php } ?>
-        </div>
+  <?php foreach ($categories_resources as $categorie) { ?>
+    @if(count($categorie->resources))
+    <div class="group" style="border-left: solid 5px #{{$categorie->color}}">
+      <h5 class="group-heading">{{$categorie->name}}</h5>
+      <div class="group-items">
+        <?php
+        foreach ($categorie->resources as $resource) {
+        ?>
+          <a href="">{{$resource->name}}
+            <p>{{$resource->desc}}</p>
+          </a>
+        <?php } ?>
       </div>
-  <?php }
-  } ?>
+    </div>
+    @endif
+  <?php } ?>
 </div>
+@else
+<p id="notFound">No API's Found</p>
+@endif
 </div>
 @endsection
