d04345b96a7b3e0bc7c018ae7cd692744554eca3
Sudhir Dhawle <dhawlesudhir@gmail.com>
2022-09-20 16:13:00 +0530

    DB code removed

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -18,54 +18,62 @@ class CategoryController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function index(Request $request, String $type = 'default')
+    //  index with db data
+    // public function index(Request $request, String $type = 'default')
+    // {
+    //     $search = $request->query('search');
+    //     if ($search) {
+    //         // $resourcedata = resource::select('id')
+    //         //     // ->where('status', 1)
+    //         //     ->where('name', 'like', '%' . $search . '%')
+    //         //     ->get();
+    //         $resourcedata = DB::table('resources')
+    //             // ->where('status', 1)
+    //             ->where('name', 'like', '%' . $search . '%')
+    //             ->get('id');
+
+    //         // dd($resourcedata->pluck('id'));
+
+    //         // $categoryids = DB::table('category_resource')
+    //         //     ->select('category_id')
+    //         //     ->whereIn('resource_id', $resourcedata)
+    //         //     ->get();
+    //         if (isset($resourcedata)) {
+    //             $categoryids = DB::table('category_resource')
+    //                 ->whereIn('resource_id', $resourcedata->pluck('id'))
+    //                 ->get('category_id');
+    //         }
+    //         // dd($categoryids);
+
+
+
+
+    //         if (isset($categoryids)) {
+    //             $categoryids =  $categoryids->pluck('category_id');
+    //             $categoriesData = category::where('status', 1)
+    //                 // ->where('name', 'like', '%' . $search . '%')
+    //                 ->whereIn('id', $categoryids)
+    //                 ->get();
+    //         } else {
+    //             $categoriesData = category::where('status', 1)->get();
+    //         }
+    //     } else {
+    //         $categoriesData = category::where('status', 1)->get();
+    //     }
+
+    //     if ($type == 'default') {
+    //         return view('layouts.main')->with('categories_resources', $categoriesData);
+    //     } else {
+    //         return $categoriesData;
+    //     }
+    // }
+
+    public function index()
     {
-        $search = $request->query('search');
-        if ($search) {
-            // $resourcedata = resource::select('id')
-            //     // ->where('status', 1)
-            //     ->where('name', 'like', '%' . $search . '%')
-            //     ->get();
-            $resourcedata = DB::table('resources')
-                // ->where('status', 1)
-                ->where('name', 'like', '%' . $search . '%')
-                ->get('id');
 
-            // dd($resourcedata->pluck('id'));
-
-            // $categoryids = DB::table('category_resource')
-            //     ->select('category_id')
-            //     ->whereIn('resource_id', $resourcedata)
-            //     ->get();
-            if (isset($resourcedata)) {
-                $categoryids = DB::table('category_resource')
-                    ->whereIn('resource_id', $resourcedata->pluck('id'))
-                    ->get('category_id');
-            }
-            // dd($categoryids);
-
-
-
-
-            if (isset($categoryids)) {
-                $categoryids =  $categoryids->pluck('category_id');
-                $categoriesData = category::where('status', 1)
-                    // ->where('name', 'like', '%' . $search . '%')
-                    ->whereIn('id', $categoryids)
-                    ->get();
-            } else {
-                $categoriesData = category::where('status', 1)->get();
-            }
-        } else {
-            $categoriesData = category::where('status', 1)->get();
-        }
-
-        if ($type == 'default') {
-            return view('layouts.main')->with('categories_resources', $categoriesData);
-        } else {
-            return $categoriesData;
-        }
+        return view('layouts.main');
     }
+    /*
     /**
      * Show the form for creating a new resource.
      *
